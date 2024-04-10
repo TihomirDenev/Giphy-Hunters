5795b417a7afa0a5faa0425315046f8c035cb631
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-10 21:19:16 +0300

    Add Favorites View

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -1,13 +1,12 @@
 import { toGifSimple } from './gif-views.js';
 
-export const toFavoritesGifs = (gifs) => `
+export const toFavoritesView = (gifs) => `
 <div id="gifs">
   <h1>Favorite gifs:</h1>
   <div class="content">
-    ${
-      gifs.map(toGifSimple).join('\n') ||
-      '<p>Add some gifs to favorites to see them here.</p>'
-    }
+    ${//gifs.map(toGifSimple).join('\n') ||
+  '!!!!TODO!!!! <p>Add some gifs to favorites to see them here.</p>'
+  }
   </div>
 </div>
 `;
