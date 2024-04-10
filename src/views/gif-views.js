e06c7c0f0d0500d3f51fbdfdb862729734fd2535
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-10 21:18:17 +0300

    Add gif-view.js

--- src/views/gif-views.js
+++ src/views/gif-views.js
@@ -1,13 +1,13 @@
-import { renderFavoriteStatus } from '../events/favorites-events.js';
+// import { renderFavoriteStatus } from '../events/favorites-events.js';
 
 export const toSingleGifView = (gif) => `
 <!-- your template here, you can use toGifDetailed(gif) -->
 `;
 
 export const toGifSimple = (gif) => `
-<!-- your template here -->
+<div>Test toGifSimple</div>
 `;
 
 const toGifDetailed = (gif) => `
-<!-- your template here -->
+<div>Test toGifSimple</div>
 `;
