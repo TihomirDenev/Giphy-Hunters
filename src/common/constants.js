06a2cac7fe87ded452350ec7bc2264510ba4d875
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-10 17:42:51 +0300

    Add missing const in constance.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,12 @@
 export const HOME = 'home';
 
+export const TRENDING_GIFS = 'trending-gifs';
+
 export const FAVORITES = 'favorites';
+
+export const UPLOAD_GIF = 'upload-gif';
+
+export const UPLOADED_GIFS = 'uploaded-gifs';
 
 export const ABOUT = 'about';
 
@@ -9,3 +15,5 @@ export const CONTAINER_SELECTOR = '#container';
 export const FULL_HEART = '❤';
 
 export const EMPTY_HEART = '♡';
+
+export const API_KEY = 'Nr5dfIBdSQVMZ7sy0p9cUTNtAZUqc6zX';
