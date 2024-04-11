12ae2496aabcae6770b38e41a9abfd47e5cd1c21
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-11 15:11:04 +0300

    Refactor

--- src/views/search-view.js
+++ src/views/search-view.js
@@ -4,10 +4,7 @@ export const toSearchView = (gifs, searchTerm) => `
 <div id="gifs">
   <h1>Gifs found for "${searchTerm}":</h1>
   <div class="content">
-    ${
-      gifs.map(toGifSimple).join('\n') ||
-      '<p>Add some gifs to favorites to see them here.</p>'
-    }
+    ${gifs.map(toGifSimple).join('\n') || '<p>Add some gifs to favorites to see them here.</p>'}
   </div>
 </div>
 `;
