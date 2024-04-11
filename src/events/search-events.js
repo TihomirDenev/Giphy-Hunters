c9d50caf4c0512832fc1c2c6b9c41059e9735946
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-11 15:03:48 +0300

    Add renderSearchItemsAsync logic

--- src/events/search-events.js
+++ src/events/search-events.js
@@ -1,4 +1,9 @@
+import { CONTAINER_SELECTOR } from "../common/constants.js";
+import { searchGifsAsync } from "../data/fetch-gifs.js";
+import { toSearchView } from "../views/search-view.js";
+import { q } from "./helpers.js";
 
-export const renderSearchItems = (searchTerm) => {
-  // missing implementation
+export const renderSearchItemsAsync = async (searchTerm) => {
+  const gifs = await searchGifsAsync(searchTerm);
+  q(CONTAINER_SELECTOR).innerHTML = toSearchView(gifs, searchTerm);
 };
