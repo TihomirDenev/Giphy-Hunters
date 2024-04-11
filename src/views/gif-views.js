18177b9f27568c5f11c797530e6f8416efe5df68
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-11 13:07:06 +0300

    Add gif view (not finished)

--- src/views/gif-views.js
+++ src/views/gif-views.js
@@ -1,13 +1,31 @@
-// import { renderFavoriteStatus } from '../events/favorites-events.js';
+import { renderFavoriteStatus } from '../events/favorites-events.js';
 
 export const toSingleGifView = (gif) => `
-<!-- your template here, you can use toGifDetailed(gif) -->
+<div id="gif">
+    <h1>${gif.title}</h1>
+    <div class="content">
+    ${toGifDetailed(gif)}
+    </div>
+</div>
 `;
 
 export const toGifSimple = (gif) => `
-<div>Test toGifSimple</div>
+<div class="gif">
+    <img src="${gif}" alt="missing image"><br>
+    <button class="view-gif-btn" data-gif-id="${gif.id}">View Gif details</button>
+    ${renderFavoriteStatus(gif.id)}
+</div>
 `;
 
 const toGifDetailed = (gif) => `
-<div>Test toGifSimple</div>
+<div class="gif-detailed">
+    <div class="poster">
+    <img src="${gif}">
+    </div>
+    <div class="gif-info">
+    <p>Type: ${gif.type}</p>
+    <p>Username: ${gif.username}</p>
+    <p>Import Data: ${gif.import_datetime}</p>
+    </div>
+</div>
 `;
