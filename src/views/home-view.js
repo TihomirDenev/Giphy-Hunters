c0e019d8bac5f2c109ad43576a169fc5f1d498a9
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-11 11:32:31 +0300

    Update home view

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,14 +1,8 @@
-export const toHomeView = () => `
+export const toHomeView = (URL) => `
 <div id="home">
   <h1>Giphy Hunters</h1>
   <div class="content">
-    <p>Simple gif database app. You can:</p>
-    <ul>
-      <li>Browse trending gifs</li>
-      <li>Upload gifs</li>
-      <li>Add and remove gifs from favorites</li>
-      <li>Search for gifs</li>
-    </ul>
+  <iframe src="https://giphy.com/embed/VbnUQpnihPSIgIXuZv" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/computer-cat-wearing-glasses-VbnUQpnihPSIgIXuZv">via GIPHY</a></p>
   </div>
 </div>
 `;
