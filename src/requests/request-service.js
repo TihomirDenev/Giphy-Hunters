7194ec28f1a2b2e82c14d7caa9dcaa71d092dba1
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-12 11:49:13 +0300

    rename func loadGifs, loadGifDetail, getTrendyGif

--- src/requests/request-service.js
+++ src/requests/request-service.js
@@ -1,8 +1,8 @@
-import { getGifs, getGifsById, getGifsDetailedInfo, getTrendyGif, } from '../data/fetch-gifs.js';
+import { getGifsAsync, getGifsById, getGifsDetailedInfoAsync, getTrendyGifAsync, } from '../data/fetch-gifs.js';
 
 // request for getting the gif and return it
-export const loadGifs = async () => {
-  const imageData = await getGifs();
+export const loadGifsAsync = async () => {
+  const imageData = await getGifsAsync();
   return imageData;
 };
 
@@ -13,14 +13,14 @@ export const loadGifsById = (id = null) => {
 };
 
 // after view details button - the information for gif
-export const loadGifDetail = async (id = null) => {
-  const gif = await getGifsDetailedInfo(id);
+export const loadGifDetailAsync = async (id = null) => {
+  const gif = await getGifsDetailedInfoAsync(id);
   return gif;
 };
 
 // request for getting trendy gifs and return them
 
 export const loadTrendyGifAsync = async () => {
-  const trendyGifs = await getTrendyGif();
+  const trendyGifs = await getTrendyGifAsync();
   return trendyGifs;
 };
