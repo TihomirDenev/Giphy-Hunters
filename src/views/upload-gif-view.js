e1c92e0619a6849a766d36f8a242772b7cbbcc0d
TanyaZhecheva <jechevatanq@gmail.com>
2024-04-12 15:41:12 +0300

    add url button

--- src/views/upload-gif-view.js
+++ src/views/upload-gif-view.js
@@ -1,7 +1,12 @@
+const url =
+  'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzdkYmdrMTd6MG1xOXVvMmhmaHU4Z3owbzB4OTBoem5scmtuZWRubyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MuJDYvbYKzMwONKvdO/giphy.gif';
+
 export const toUploadGifView = () => `
 <div id="about">
   <div class="content">
     <h1>Upload gif view</h1>
+    <input type="file" id="fileInput" accept=".gif" />
+    <button class="upload" data-url="${url}">Upload</button>
   </div>
 </div>
 `;
