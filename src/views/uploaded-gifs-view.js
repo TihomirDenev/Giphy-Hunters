0617fa71017295eea579bf6514a57cafdfb3b652
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-12 20:10:03 +0300

    Change the preview of uploaded gifs

--- src/views/uploaded-gifs-view.js
+++ src/views/uploaded-gifs-view.js
@@ -1,13 +1,14 @@
+import { UPLOADED_GIFS } from '../common/constants.js';
+import { toGifSimple } from './gif-views.js';
 export const toUploadedGifsView = (gifs) => `
 <div id="about">
   <div class="content">
-    <h1>Uploaded Gifs View</h1>
-     ${
-       gifs.map(
-         (gif) =>
-           `<img src="${gif.images.original.url}" alt="missing image"><br>`
-       ) || `<p>No Uploads</p>`
-     }
+    <h1>Animated Treasures Uploaded by Me</h1>
+    <div class="gif-listing">${gifs.map((gif) => toGifSimple(gif, UPLOADED_GIFS)).join('') || `
+    <h2>No GIFs uploaded yet!</h2>
+    <h2>GIF gallery is waiting for your contributions!</h2>
+    `
+  }</div>
   </div>
 </div>
 `;
