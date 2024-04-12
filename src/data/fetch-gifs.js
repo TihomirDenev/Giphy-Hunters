5a16ecc38bf001ca6af64d11d8bc277945f3daea
TanyaZhecheva <jechevatanq@gmail.com>
2024-04-12 15:43:44 +0300

    post request for gif

--- src/data/fetch-gifs.js
+++ src/data/fetch-gifs.js
@@ -3,7 +3,9 @@
 import { API_KEY } from '../common/constants.js';
 
 export const getGifsAsync = async () => {
-  const randomGif = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+  const randomGif = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+  );
   const data = await randomGif.json();
   const imageData = data.data;
   return imageData;
@@ -15,20 +17,45 @@ export const getGifsById = (gifId = null) => {
 };
 
 export const getGifsDetailedInfoAsync = async (gifId = null) => {
-  const gifByID = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${API_KEY}`);
+  const gifByID = await fetch(
+    `https://api.giphy.com/v1/gifs/${gifId}?api_key=${API_KEY}`
+  );
   const data = await gifByID.json();
   return data.data;
 };
 
 export const searchGifsAsync = async (searchTerm = '') => {
-  const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}`);
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}`
+  );
   const dataJson = await response.json();
   return dataJson.data;
 };
 
 export const getTrendyGifAsync = async () => {
-  const data = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`);
+  const data = await fetch(
+    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
+  );
 
   const getTrendyGif = await data.json();
   return getTrendyGif;
+};
+
+export const postGifAsync = async (bodyUrl) => {
+  console.log('POST GIF ASYNC');
+
+  const bodyData = {
+    source_image_url: bodyUrl,
+  };
+
+  const data = await fetch(
+    `https://upload.giphy.com/v1/gifs?api_key=${API_KEY}`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(bodyData),
+    }
+  );
+  const response = await data.json();
+  console.log(response);
 };
