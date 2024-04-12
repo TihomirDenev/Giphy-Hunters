939b6b4c8ef723c2d30f28bb55fed6b261909f39
TanyaZhecheva <jechevatanq@gmail.com>
2024-04-12 17:53:57 +0300

    fix the uploaded gif`s view

--- src/views/uploaded-gifs-view.js
+++ src/views/uploaded-gifs-view.js
@@ -1,7 +1,13 @@
-export const toUploadedGifsView = () => `
+export const toUploadedGifsView = (gifs) => `
 <div id="about">
   <div class="content">
     <h1>Uploaded Gifs View</h1>
+     ${
+       gifs.map(
+         (gif) =>
+           `<img src="${gif.images.original.url}" alt="missing image"><br>`
+       ) || `<p>No Uploads</p>`
+     }
   </div>
 </div>
 `;
