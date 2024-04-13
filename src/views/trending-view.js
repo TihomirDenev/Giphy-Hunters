5fd6552d7d5cda40487abbaebd2f1bb7c27681c7
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-14 02:04:53 +0300

    Edit footer to be p tag

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -5,10 +5,13 @@ export const toTrendingView = (gifs) => `
 <div id="Trendy">
   <div class="content">
     <h1>TrendyGIFHub</h1>
-    <footer>Welcome to TrendyGIFHub, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</footer>
-    <div class="gif-listing">${gifs.data
-      .map((gif) => toGifSimple(gif, TRENDING_GIFS))
-      .join('')}</div>
+    <p>Welcome to TrendyGIFHub, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</p>
+    <div class="gif-listing">
+    ${gifs.data
+    .map((gif) => toGifSimple(gif, TRENDING_GIFS))
+    .join('')
+  }
+    </div>
   </div>
 </div>
 `;
