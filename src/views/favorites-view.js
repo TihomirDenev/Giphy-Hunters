bb3d472427256234fc15755e4774729ea1e1db2d
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-13 10:34:00 +0300

    Update favorite view

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -1,12 +1,13 @@
-import { toGifSimple } from './gif-views.js';
+import { toGifSimple } from "./gif-views.js";
 
-export const toFavoritesView = (gifs) => `
+export const toFavoritesView = (gifs) =>`
 <div id="gifs">
-  <h1>Favorite gifs:</h1>
   <div class="content">
-    ${//gifs.map(toGifSimple).join('\n') ||
-  '!!!!TODO!!!! <p>Add some gifs to favorites to see them here.</p>'
-  }
+  <h1>Favorite gifs:</h1>
+  ${gifs.map(toGifSimple).join('\n') || `
+  <h2>No GIFs added in favorites yet!</h2>
+  <h2>GIF favorites is waiting for your contributions!</h2>
+  `}
   </div>
 </div>
 `;
