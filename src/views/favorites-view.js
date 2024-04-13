4ac1d5250553cedcd6c27754ce862f7289dff8af
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-13 10:58:48 +0300

    Added new API KEY And fix back button for favorites

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -1,13 +1,14 @@
 import { toGifSimple } from "./gif-views.js";
+import { FAVORITES } from '../common/constants.js';
 
 export const toFavoritesView = (gifs) =>`
-<div id="gifs">
-  <div class="content">
+<div id="about">
+<div class="content">
   <h1>Favorite gifs:</h1>
-  ${gifs.map(toGifSimple).join('\n') || `
+  <div class="gif-listing">${gifs.map((gif) => toGifSimple(gif, FAVORITES)).join('') || `
   <h2>No GIFs added in favorites yet!</h2>
   <h2>GIF favorites is waiting for your contributions!</h2>
-  `}
+  `}</div>
   </div>
 </div>
 `;
