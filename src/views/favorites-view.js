446e7413f35263450e853c317c486aadc2b7b69b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-14 00:09:25 +0300

    Rename id from about to favorites

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -1,8 +1,8 @@
 import { toGifSimple } from "./gif-views.js";
 import { FAVORITES } from '../common/constants.js';
 
-export const toFavoritesView = (gifs) =>`
-<div id="about">
+export const toFavoritesView = (gifs) => `
+<div id="favorite">
 <div class="content">
   <h1>Favorite gifs:</h1>
   <div class="gif-listing">${gifs.map((gif) => toGifSimple(gif, FAVORITES)).join('') || `
