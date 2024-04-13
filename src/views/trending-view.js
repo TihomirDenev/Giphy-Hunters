10caa877f87a2bd7252a4cdbdfbbff1a56851f4c
TanyaZhecheva <jechevatanq@gmail.com>
2024-04-13 14:52:33 +0300

    add footer to style

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -5,8 +5,10 @@ export const toTrendingView = (gifs) => `
 <div id="Trendy">
   <div class="content">
     <h1>TrendyGIFHub</h1>
-    <p>Welcome to TrendyGIFHub, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</p>
-    <div class="gif-listing">${gifs.data.map((gif) => toGifSimple(gif, TRENDING_GIFS)).join('')}</div>
+    <footer>Welcome to TrendyGIFHub, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</footer>
+    <div class="gif-listing">${gifs.data
+      .map((gif) => toGifSimple(gif, TRENDING_GIFS))
+      .join('')}</div>
   </div>
 </div>
 `;
