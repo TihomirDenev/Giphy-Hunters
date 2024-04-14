0bbed3ae624667e038d2681c44dea16b777f6c3c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-14 22:33:50 +0300

    Add more info

--- src/views/upload-gif-view.js
+++ src/views/upload-gif-view.js
@@ -1,10 +1,14 @@
 export const toUploadGifView = () => `
 <div id="about">
   <div class="content">
-    <h1>Upload gif view</h1>
-    <form id="uploadForm" class="upload">
-      <input type="file" id="fileInput" accept=".gif" />
-      <button class="upload" type="submit">Upload GIF</button>
+    <h1>Upload your own gif</h1>
+    <br />
+    <p>Share your gif with the world!</p>
+    <p>Upload your gif and see it in the <span class="golden-text">Uploaded Gifs</span> section.</p>
+    <br />
+    <form id="uploadForm" class="upload ">
+      <input type="file" id="fileInput" accept=".gif" placeholder="Upload" />
+      <button class="upload button-golden" type="submit">Upload GIF</button>
     </form>
   </div>
 </div>
