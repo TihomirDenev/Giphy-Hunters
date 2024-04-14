5e314169e522f90ebe58369d9fb520f9f0d1edb2
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-14 21:17:06 +0300

    Add About text

--- src/views/about-view.js
+++ src/views/about-view.js
@@ -2,8 +2,47 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors: Tanchity, Tisho, Rado </h2>
-    <h2>Date: 2024</h2>
+    <p>
+      This app is a simple web application that uses the Giphy API to display gifs. The app allows you to view gifs,
+      search for gifs, view details of a gif, and add gifs to your favorites. You can also upload your own gifs to the
+      app.
+    </p>
+    <h2>Features</h2>
+    <ul>
+      <li>View gifs</li>
+      <li>Search for gifs</li>
+      <li>View details of a gif</li>
+      <li>Add gifs to favorites</li>
+      <li>Upload your own gifs</li>
+    </ul>
+    <h2>Technologies</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+    <h2>API</h2>
+    <p>
+      The app uses the Giphy API to fetch gifs. You can find more information about the Giphy API at
+      <a class="nav-link" href="https://developers.giphy.com/docs/api#quick-start-guide" target="_blank">Giphy API</a>.
+    </p>
+
+    <h2>Author</h2>
+    <p>
+      This app was created by  <br />
+      <a class="nav-link" href="mailto:radoslav.marinov.a58@learn.telerikacademy.com"
+      >Tihomir Denev @ Telerik Academy 
+    </a> 
+    <br />
+    <a class="nav-link" href="mailto:jechevatanq@learn.telerikacademy.com">
+      Tanya Zhecheva @ Telerik Academy 
+    </a> 
+    <br />
+    </a>
+    <a class="nav-link" href="mailto:radoslav.marinov.a58@learn.telerikacademy.com"
+      >Radoslav Marinov @ Telerik Academy
+    </a>
+    </p>
   </div>
 </div>
 `;
