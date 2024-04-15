5d8671d47a760410cca2e175b33857e417b68eb3
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-15 13:07:16 +0300

    Add functionality to back btn in search view (view details)

--- src/views/search-view.js
+++ src/views/search-view.js
@@ -3,6 +3,6 @@ import { toGifSimple } from './gif-views.js';
 export const toSearchView = (gifs, searchTerm) => `
 <div id="gifs">${searchTerm ? `<h1>Gifs found for "${searchTerm}":</h1>
 <div class="content">
-${gifs.map(gif => toGifSimple(gif)).join('\n') || '<p>No Gifs Found</p>'}
+<div class="gif-listing">${gifs.map(gif => toGifSimple(gif)).join('\n') || '<p>No Gifs Found</p>'}</div>
 </div>`: '<h1>Search view page</h1>'}
 </div>`;
