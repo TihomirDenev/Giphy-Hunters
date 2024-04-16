6d512be6aa09d4c529e081302a456093e4ef6e94
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-04-16 22:12:00 +0300

    Edit view

--- src/views/trending-view.js
+++ src/views/trending-view.js
@@ -4,8 +4,8 @@ import { toGifSimple } from './gif-views.js';
 export const toTrendingView = (gifs) => `
 <div id="Trendy">
   <div class="content">
-    <h1>TrendyGIFHub</h1>
-    <p>Welcome to TrendyGIFHub, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</p>
+    <h1>Trendy GIFS</h1>
+    <p>Welcome to Trendy GIFS, the ultimate GIF repository designed exclusively for programmers like you! Dive into a world where code meets creativity, and discover a curated collection of trending GIFs that perfectly capture the essence of the programming experience.</p>
     <div class="gif-listing">
     ${gifs.data
     .map((gif) => toGifSimple(gif, TRENDING_GIFS))
