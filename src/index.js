cec1871060929b114514e9ae932f07163989ad50
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-04-16 13:17:23 +0300

    Add func for light theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { HOME } from './common/constants.js';
 import { q } from './events/helpers.js';
-import { loadPage, renderGifDetailsAsync } from './events/navigation-events.js';
+import { loadPage, renderGifDetailsAsync, renderVideoView } from './events/navigation-events.js';
 import { renderSearchItemsAsync } from './events/search-events.js';
 import { toggleFavoriteStatus } from './events/favorites-events.js';
 import { uploadGifAsync } from './requests/request-service.js';
@@ -54,6 +54,11 @@ document.addEventListener('DOMContentLoaded', () => {
       if (event.key === 'Enter') {
         renderSearchItemsAsync(event.target.value);
       }
+    });
+
+    const lightThemeBtn = document.getElementById('light-theme');
+    lightThemeBtn.addEventListener('click', () => {
+      renderVideoView();
     });
   });
 
